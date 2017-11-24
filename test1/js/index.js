5b4691d7773660971c454cce4eb4b29e14d0ad29
vasily-d <den771981@mail.ru>
2017-11-24 21:27:57 +0300

    add test1 site

---
+++ test1/js/index.js
@@ -0,0 +1,111 @@
+$(function() {
+
+	// 1 - slider
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+	  speed: 800
+	});
+
+	// 2 - two popups in left menu
+	var a = $('.js-ul li a');
+
+	a.on('click', function(e) {
+		e.preventDefault();
+		// console.log($(this).attr('data-number'));
+
+		switch($(this).attr('data-number')) {
+			case '1':
+				// hide all items in left menu
+				$('main .menu6').fadeOut(0);
+				// add new menuN pages popup when create
+				//
+				//
+
+				// $('main .menu1').css('display', 'block');
+				$('main .menu1').fadeToggle(0, 'linear', verifeMenuN);
+				break;
+
+			case '2':
+				// alert('2');
+				break;
+
+			case '3':
+				// alert('3');
+				break;
+
+			case '4':
+				// alert('4');
+				break;
+
+			case '5':
+				// alert('5');
+				break;
+
+			case '6':
+				// hide all items in left menu
+				$('main .menu1').fadeOut(0);
+				// add new menuN pages popup when create
+				//
+				//
+
+				// $('main .menu2').css('display', 'block');
+				$('main .menu6').fadeToggle(0, 'linear', verifeMenuN);
+				break;
+
+			case '7':
+				// alert('7');
+				break;
+		}
+
+
+		function verifeMenuN() {
+			// block verife in menu1... menuN (display: block/none) for overlay
+			if(
+					($('main .menu1').css('display') == 'block')
+					|| ($('main .menu6').css('display') == 'block')
+					// add new if if create popup
+					//
+					//
+				)
+			{
+				$('main .overlay').fadeIn(0);
+				// $(this).fadeIn(0);
+			}
+			else{
+				$('main .overlay').fadeOut(0);
+			}
+		}
+	});
+
+	// 3 - gumburger click
+	$('header .header-info .icon').on('click', function() {
+		$('.main-content .left-menu').addClass('fix-menu');
+		$('.main-content .left-menu').animate({left: "0"}, 300);
+
+		// block сдвига текста попап при открытии left меню
+		$('.main-content main .menu1').css('padding-left', '212px');
+		//
+		//		
+		$('.main-content main .menu6').css('padding-left', '212px');
+	});
+
+	// 4 - click to close gumburger
+	$('.close-menu-popup').on('click', function() {
+		$('.main-content .left-menu').animate({left: "-200px"}, 300, function() {
+			$('.main-content .left-menu').removeClass('fix-menu');
+				$('main .all-menu').fadeOut(0);
+				$('.overlay').fadeOut(0);
+		});
+
+		// block сдвига текста попап при скрытии left меню
+		$('.main-content main .menu1').css('padding-left', '110px');
+		//
+		//
+		$('.main-content main .menu6').css('padding-left', '110px');
+	});
+});
