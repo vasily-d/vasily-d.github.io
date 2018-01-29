f953e3efae4c01189d61c1189dc6c3e62b8326d6
vasily-d <den771981@mail.ru>
2018-01-29 13:35:10 +0300

    add markup

---
+++ test7/js/index-page.js
@@ -0,0 +1,83 @@
+$(function() {
+	$(document).ready(function() {
+		// slick slider
+		$('.slider .slider-slick').slick({
+			slidesToShow: 1,
+			autoplay: true,
+  		autoplaySpeed: 2000,
+			arrows: true,
+			dots: true
+		});
+
+
+		
+
+		// slick slider2
+		$('.slider2 .slider-slick').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			infinite: true,
+			autoplay: true,
+  		autoplaySpeed: 2000,
+			arrows: true,
+			dots: true
+		});
+
+
+		 $(".owl-carousel").owlCarousel({
+		 	items: 2,
+		 	loop: true,
+		 	autoWidth: true,
+		 	margin: 30,
+		 	autoplay: true,
+		 	nav: true,
+		 	dots: true
+		 });
+
+
+		// popups
+		// ставим на кнопку которая вызывает данный popup
+		$('.js-popup').on('click', function(e) {
+			e.preventDefault();
+
+			// поиск button close popup
+			$('#js-close-popup').on('click', function(e) {
+				// $('.popup1').css('display', 'none');
+				// $('.popup-overlay1').css('display', 'none');
+				// or
+				$('.popup1').slideUp();
+				$('.popup-overlay1').css('display', 'none');
+			});
+
+			// поиск popup и popup-overlay для их открытия
+			// $('.popup1').css('display', 'block');
+			// $('.popup-overlay1').css('display', 'block');
+			// or
+			$('.popup1').slideDown();
+			$('.popup-overlay1').css('display', 'block');
+		});
+
+
+		// ставим на кнопку которая вызывает данный popup
+		$('.js-popup2').on('click', function(e) {
+			e.preventDefault();
+
+			// поиск button close popup
+			$('#js-close-popup2').on('click', function(e) {
+				// $('.popup2').css('display', 'none');
+				// $('.popup-overlay1').css('display', 'none');
+				// or
+				$('.popup2').slideUp();
+				$('.popup-overlay1').css('display', 'none');
+			});
+
+			// поиск popup и popup-overlay для их открытия
+			// $('.popup1').css('display', 'block');
+			// $('.popup-overlay1').css('display', 'block');
+			// or
+			$('.popup2').slideDown();
+			$('.popup-overlay1').css('display', 'block');
+		});	
+
+	});
+});
