9c23befb418d69ca59ee310f5d70ef510e9fa345
vasily-d <den771981@mail.ru>
2018-02-13 23:44:26 +0300

    add site

---
+++ test10/js/index-page.js
@@ -0,0 +1,26 @@
+$(function() {
+	$(document).ready(function() {
+		// slick slider1
+		$('.slider1 .slider-slick').slick({
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			arrows: true,
+			dots: false
+		});
+
+		// slick slider2
+		$('.slider2 .slider-slick').slick({
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			arrows: true,
+			dots: false
+		});
+
+
+
+	});
+});
