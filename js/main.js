820d2d814f131ba727a43346800e216908299f5e
vdev <den7781@mail.ru>
2018-10-26 15:53:39 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
 	  
 		if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 900); // анимируем скроолинг к элементу scroll_el
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 700); // анимируем скроолинг к элементу scroll_el
 		}
 
 		return false; // выключаем стандартное действие
